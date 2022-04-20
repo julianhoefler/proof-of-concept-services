8fe9ab9be4f25e13ba2a7a650e541e3d5ae6c556
Julian Höfler <julhoefler@gmail.com>
2022-04-20 17:31:29 +0200

    fix MapResolverTest

--- src/test/java/de/abschlussprojekt/core/resolver/MapResolverTest.java
+++ src/test/java/de/abschlussprojekt/core/resolver/MapResolverTest.java
@@ -5,8 +5,9 @@ import de.abschlussprojekt.dmadapter.FileNameResolver;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.HashMap;
@@ -19,20 +20,21 @@ class MapResolverTest {
     private static final Map<String, List<JourneyDetails>> journeyDetailsMap = new HashMap<>();
     private static final String DIRECTORY = "C:\\Users\\JulianHoefler\\Projekte\\eigeneProjekte\\proof-of-concept-services\\src\\main\\resources\\data\\journeydetails";
 
-    @Autowired
+    @Mock
+    FileNameResolver fileNameResolverMock;
+
+    @InjectMocks
     MapResolver mapResolver;
 
     @BeforeAll
     public static void setup() {
         journeyDetailsMap.put(DIRECTORY, List.of(new JourneyDetails()));
-
-        FileNameResolver fileNameResolverMock = Mockito.mock(FileNameResolver.class);
-        Mockito.when(fileNameResolverMock.resolvePath("key", "subDirectory")).thenReturn(DIRECTORY);
-//        QuarkusMock.installMockForType(fileNameResolverMock, FileNameResolver.class);
     }
 
     @Test
     void resolve_shouldReturnCorrectList() {
+        Mockito.when(fileNameResolverMock.resolvePath("key", "subDirectory")).thenReturn(DIRECTORY);
+
         List<JourneyDetails> result = mapResolver.resolve(journeyDetailsMap, "key", "subDirectory");
 
         Assertions.assertTrue(journeyDetailsMap.containsValue(result));
